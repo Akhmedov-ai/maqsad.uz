87494be8b40b58d4a971342efff3ae8a99599e74
ameerarx <ameerarx@outlook.com>
2021-12-04 13:14:52 +0500

    dssd

---
+++ data/icons.js
@@ -0,0 +1,27 @@
+const home = require('../assets/home.png')
+const order = require('../assets/order.png')
+const notification = require('../assets/notification.png')
+const menu = require('../assets/menu.png')
+const warning = require('../assets/warning.png')
+const add = require('../assets/add.png')
+const card = require('../assets/card.png')
+const check = require('../assets/check.png')
+const copy = require('../assets/copy.png')
+const activeHome = require('../assets/activeHome.png')
+const activeOrder = require('../assets/activeOrder.png')
+const activeNotification = require('../assets/activeBell.png')
+
+export default {
+    home,
+    order,
+    notification,
+    menu,
+    warning,
+    add,
+    card,
+    check,
+    copy,
+    activeHome,
+    activeOrder,
+    activeNotification,
+}
---
+++ data/theme.js
@@ -0,0 +1,45 @@
+import { Dimensions } from 'react-native'
+
+const { width, height } = Dimensions.get('window')
+
+export const COLORS = {
+    green: '#E6F5EA',
+    white2: '#FBFBFB',
+    white: '#FFFFFF',
+    black: '#000000',
+    black1: '#222628',
+    green1: '#E5E5E5',
+
+    transparent: 'transparent',
+    transparentWhite1: 'rgba(255, 255, 255, 0.1)',
+    transparentBlack1: 'rgba(0, 0, 0, 0.1)',
+    transparentBlack7: 'rgba(0, 0, 0, 0.7)',
+}
+export const SIZES = {
+    // global sizes
+    base: 8,
+    font: 14,
+    radius: 12,
+    padding: 24,
+
+    // font sizes
+    largeTitle: 40,
+    h1: 30,
+    h2: 22,
+    h3: 16,
+    h4: 14,
+    h5: 12,
+    body1: 30,
+    body2: 22,
+    body3: 16,
+    body4: 14,
+    body5: 12,
+
+    // app dimensions
+    width,
+    height,
+}
+
+const appTheme = { COLORS, SIZES }
+
+export default appTheme
